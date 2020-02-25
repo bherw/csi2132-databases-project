c301b9dd63ea85a04983c69b89e80ac9b5164ed8
Ben Herweyer <benjamin.herweyer@gmail.com>
2020-02-25 11:50:14 -0500

    Prepare for running migrations

--- lib/Csi2132/Project.pm
+++ lib/Csi2132/Project.pm
@@ -1,15 +1,21 @@
 package Csi2132::Project;
 use Mojo::Base 'Mojolicious';
+use Mojo::Pg;
 
 # This method will run once at server start
 sub startup {
   my $self = shift;
 
-  # Load configuration from hash returned by config file
+  # Config
   my $config = $self->plugin('Config');
-
-  # Configure the application
   $self->secrets($config->{secrets});
+
+  # Model
+  $self->helper(pg => sub { state $pg = Mojo::Pg->new(shift->config('pg')) });
+
+  # Migrate to latest version if necessary
+  my $path = $self->home->child('migrations', 'project.sql');
+  $self->pg->auto_migrate(1)->migrations->name('project')->from_file($path);
 
   # Router
   my $r = $self->routes;
