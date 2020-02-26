70b44894a510003c15de39422ea67cc43f6cee66
Ben Herweyer <benjamin.herweyer@gmail.com>
2020-02-26 11:30:41 -0500

    Autoformatting

--- lib/Csi2132/Project.pm
+++ lib/Csi2132/Project.pm
@@ -4,38 +4,38 @@ use Mojo::Pg;
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Config
-  my $config = $self->plugin('Config');
-  $self->secrets($config->{secrets});
+    # Config
+    my $config = $self->plugin('Config');
+    $self->secrets($config->{secrets});
 
-  # Model
-  $self->helper(pg => sub { state $pg = Mojo::Pg->new(shift->config('pg')) });
+    # Model
+    $self->helper(pg => sub {state $pg = Mojo::Pg->new(shift->config('pg'))});
 
-  # Migrate to latest version if necessary
-  my $path = $self->home->child('migrations', 'project.sql');
-  $self->pg->auto_migrate(1)->migrations->name('project')->from_file($path);
+    # Migrate to latest version if necessary
+    my $path = $self->home->child('migrations', 'project.sql');
+    $self->pg->auto_migrate(1)->migrations->name('project')->from_file($path);
 
-  # Router
-  my $r = $self->routes;
-  $r->namespaces(['Csi2132::Project::Controller']);
+    # Router
+    my $r = $self->routes;
+    $r->namespaces([ 'Csi2132::Project::Controller' ]);
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    # Normal route to controller
+    $r->get('/')->to('example#welcome');
 
-  # Test Queries
-  $r->get('/test')->to('test_queries#index');
-  $r->get('/test/one')->to('test_queries#query_one');
-  $r->get('/test/two')->to('test_queries#query_two');
-  $r->get('/test/three')->to('test_queries#query_three');
-  $r->get('/test/four')->to('test_queries#query_four');
-  $r->get('/test/five')->to('test_queries#query_five');
-  $r->get('/test/six')->to('test_queries#query_six');
-  $r->get('/test/seven')->to('test_queries#query_seven');
-  $r->get('/test/eight')->to('test_queries#query_eight');
-  $r->get('/test/nine')->to('test_queries#query_nine');
-  $r->get('/test/ten')->to('test_queries#query_ten');
+    # Test Queries
+    $r->get('/test')->to('test_queries#index');
+    $r->get('/test/one')->to('test_queries#query_one');
+    $r->get('/test/two')->to('test_queries#query_two');
+    $r->get('/test/three')->to('test_queries#query_three');
+    $r->get('/test/four')->to('test_queries#query_four');
+    $r->get('/test/five')->to('test_queries#query_five');
+    $r->get('/test/six')->to('test_queries#query_six');
+    $r->get('/test/seven')->to('test_queries#query_seven');
+    $r->get('/test/eight')->to('test_queries#query_eight');
+    $r->get('/test/nine')->to('test_queries#query_nine');
+    $r->get('/test/ten')->to('test_queries#query_ten');
 }
 
 1;
