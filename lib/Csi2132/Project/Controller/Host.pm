7612ae95c5d47f74be49b0234ae9944a4a7e266a
Ben Herweyer <benjamin.herweyer@gmail.com>
2020-04-04 15:02:59 -0400

    Add publish/unpublish buttons to the host index

--- lib/Csi2132/Project/Controller/Host.pm
+++ lib/Csi2132/Project/Controller/Host.pm
@@ -5,7 +5,7 @@ use Csi2132::Project::DB;
 sub index($self) {
     my $person = $self->current_user;
     my $properties = $self->db->query(qq{
-        SELECT property_id, title FROM $PROPERTY WHERE host_id = ? AND NOT is_deleted
+        SELECT * FROM $PROPERTY WHERE host_id = ? AND NOT is_deleted
         }, $person->{person_id})->hashes;
     my $rental_requests = $self->db->query(qq{
         SELECT $RENTAL_REQUESTS.*,
@@ -16,6 +16,23 @@ sub index($self) {
         JOIN $PROPERTY USING ("property_id")
         WHERE host_id = ?
         }, $person->{person_id})->hashes;
+
+    for (@$properties) {
+        $_->{can_publish} = !$_->{is_published}
+            && $_->{street_address}
+            && $_->{state}
+            && $_->{country}
+            && $_->{postal_code}
+            && defined $_->{checkin_time_from}
+            && defined $_->{checkin_time_to}
+            && defined $_->{checkout_time_from}
+            && defined $_->{checkout_time_to}
+            && defined $_->{checkin_time_from}
+            && $_->{base_price}
+            && $_->{min_price}
+            && $_->{max_price}
+            && $_->{currency};
+    }
 
     $self->stash(properties => $properties);
     $self->stash(rental_requests => $rental_requests);
