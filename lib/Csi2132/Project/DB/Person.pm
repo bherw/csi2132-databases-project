49babc3ea67e9eaca2a575f7c6aff66e9643711d
Ben Herweyer <benjamin.herweyer@gmail.com>
2020-04-06 13:54:48 -0400

    Document DB::Person methods

--- lib/Csi2132/Project/DB/Person.pm
+++ lib/Csi2132/Project/DB/Person.pm
@@ -9,21 +9,26 @@ has 'pg';
 has 'person_class' => 'Csi2132::Project::Model::Person';
 has 'employee_class' => 'Csi2132::Project::Model::Employee';
 
+# Loads a single person by email
 sub load_by_email($self, $email) {
     my $attrs = $self->pg->db->query("SELECT * FROM $PERSON LEFT JOIN $EMPLOYEE USING (person_id) WHERE email=?", $email)->hash;
     return $self->_inflate($attrs);
 }
 
+# Loads a single person by id
 sub load_by_id($self, $id) {
     my $attrs = $self->pg->db->query("SELECT * FROM $PERSON LEFT JOIN $EMPLOYEE USING (person_id) WHERE person_id=?", $id)->hash;
     return $self->_inflate($attrs);
 }
 
+# Creates a new user with the given attributes and returns the new user object
+# The password must be hashed before passing it to this function
 sub register($self, $attrs) {
     $self->pg->db->insert($PERSON, $attrs);
     return $self->load_by_email($attrs->{email});
 }
 
+# inflates a hash loaded from the db of a person and their employee role if applicable
 sub _inflate($self, $attrs) {
     return unless $attrs;
     my %employee_attrs = delete %$attrs{'position', 'salary'};
